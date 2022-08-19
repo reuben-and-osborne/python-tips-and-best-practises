84c6cea9eb9770eead4e18e075caafad2a149145
and-edcox <edward.cox@and.digital>
2022-08-19 16:20:37 +0100

    added context manager

---
+++ context_manager.py
@@ -0,0 +1,17 @@
+from asyncore import file_dispatcher
+import json
+
+# BAD
+file = open("data.json")
+
+data = json.load(file)
+
+file.close()
+
+print(data)
+
+# GOOD
+with open("data.json") as file:
+    data = json.load(file)
+
+print(data)
