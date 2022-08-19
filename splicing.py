53fbc77520ba24095cb1534eef7907f743fb10c7
Reuben Osborne <reubenosborne@VWRQQFY273.local>
2022-08-19 15:11:23 +0100

    added comments to slicing.py

--- splicing.py
+++ splicing.py
@@ -1,7 +1,16 @@
 l = [i for i in range(10)]
 
+# First 5 items
 print(l[:5])
+
+# Last 5 items:
 print(l[5:])
+
+# Items 4,5,6
 print(l[4:7])
+
+# Ever other item
 print(l[::2])
+
+# Reverse the list
 print(l[::-1])
