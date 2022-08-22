f626d47b1ef716b881d34846bafbe0340bcf78c8
and-edcox <edward.cox@and.digital>
2022-08-22 16:41:02 +0100

    added iterate over two lists

---
+++ two_iter.py
@@ -0,0 +1,17 @@
+letters = ["a", "b", "c", "d", "e"]
+
+numbers = [1, 2, 3, 4, 5]
+
+# BAD
+# Better to iterate over the items
+for i in range(len(letters)):
+    print(letters[i], numbers[i])
+
+# BAD
+# You can (should) iterate over both lists
+for i, letter in enumerate(letters):
+    print(letter, numbers[i])
+
+# GOOD
+for letter, number in zip(letters, numbers):
+    print(letter, number)
