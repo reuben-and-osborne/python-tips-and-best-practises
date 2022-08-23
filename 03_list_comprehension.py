79050a5dbeb8df77cf2b361aa9a062b19a018c7c
Reuben Osborne <reubenosborne@VWRQQFY273.local>
2022-08-23 10:42:33 +0100

    added example comments

--- 03_list_comprehension.py
+++ 03_list_comprehension.py
@@ -2,6 +2,7 @@ import time
 
 nums = [1,2,3,4,5,6,7,8,9,10]
 
+# Example 1: Iteration
 start = time.perf_counter()
 
 squares = []
@@ -12,6 +13,7 @@ end = time.perf_counter()
 print(f"{squares} in {(end-start)*1000}")
 ex1_time = end-start
 
+# Example 2: Comprehension
 start = time.perf_counter()
 
 squares = [n**2 for n in nums]
@@ -21,6 +23,8 @@ print(f"{squares} in {(end-start)*1000}")
 ex2_time = end-start
 
 print(f"{round(ex1_time/ex2_time, 2)} times faster")
+
+# Example 3: Even Squares
 
 # squares = []
 # for n in nums:
