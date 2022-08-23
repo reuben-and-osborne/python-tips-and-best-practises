7b37c6586b109788495b5de381704461b1bd7a49
Reuben Osborne <reubenosborne@VWRQQFY273.local>
2022-08-23 11:03:17 +0100

    format string comments

--- 06_format_strings.py
+++ 06_format_strings.py
@@ -1,9 +1,11 @@
+# Readability in strings 
+
 animal = "dog"
 name = "Rover"
 sound = "woof"
 
+# How would you rank these in terms of readabiltiy? 
 print("My name is " +name+ ", I am a " +animal+ " and I say "+sound+".")
 print("My name is",name,", I am a",animal,"and I say",sound,".")
 print("My name is {}, I am a {} and I say {}.".format(name, animal, sound))
-
 print(f"My name is {name}, I am a {animal} and I say {sound}.")
